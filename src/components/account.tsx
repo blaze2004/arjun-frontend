d800c25ca921c3b71587cd1a4b4416dc9dd1b495
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2023-03-09 10:06:12 +0000

    phone number control added

--- src/components/account.tsx
+++ src/components/account.tsx
@@ -1,42 +1,167 @@
-import { Session } from '@supabase/auth-helpers-react';
-import { FaWhatsapp } from 'react-icons/fa';
+import "react-phone-input-2/lib/style.css";
+import PhoneInput from "react-phone-input-2";
+import { useState, useEffect } from "react";
+import {
+  Session,
+  useSupabaseClient,
+  useUser
+} from "@supabase/auth-helpers-react";
+import { FaWhatsapp } from "react-icons/fa";
+import { Database } from "@/types/database.types";
+import { useRouter } from "next/router";
 
-const Account=({ session }: { session: Session }) => {
-    const arjunWhatsAppNumber=process.env.NEXT_PUBLIC_ARJUN_WHATSAPP_NUMBER;
+type Profiles = Database["public"]["Tables"]["profiles"]["Row"];
 
-    return (
-        <div className='max-w-xl px-5 xl:px-0'>
-            <h1 className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold  text-transparent drop-shadow-sm md:text-7xl md:leading-[5rem]" style={{ opacity: 1, transform: "none" }}>
-                <span style={{ display: 'inline-block', verticalAlign: 'top', textDecoration: 'inherit', maxWidth: '541px' }}>Hi, {"User Name"}</span>
-            </h1>
-            <p className="mt-6 text-center text-gray-500 md:text-xl" style={{ opacity: 1 }}>
-                <span style={{ display: 'inline-block', verticalAlign: 'top', textDecoration: 'inherit', maxWidth: '541px' }}>Manage your schedule from WhatsApp by chatting with Arjun.</span>
-            </p>
-            <div className="m-4">
-                <label className="block text-gray-700 font-bold mb-2" htmlFor="numberInput">
-                    Your phone number
-                </label>
-                <div className="flex items-center gap-5">
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="numberInput"
-                        type="number"
-                        placeholder="Your WhatsApp number to chat with Arjun."
-                    />
-                    <button className="rounded-full border border-black bg-white p-1.5 px-4 text-sm text-black transition-all hover:bg-black hover:text-white self-center">Save</button>
-                </div>
-            </div>
+const Account = ({ session }: { session: Session }) => {
+  const arjunWhatsAppNumber = process.env.NEXT_PUBLIC_ARJUN_WHATSAPP_NUMBER;
 
-            <div className="mx-auto mt-6 flex items-center justify-center space-x-5" style={{ opacity: 1, transform: "none" }}>
-                <a
-                    href={`https://wa.me/+91${arjunWhatsAppNumber}?text=Hi+Arjun`}
-                    className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black flex"
-                >
-                    <FaWhatsapp className="mr-2" size={20} />  Chat with Arjun
-                </a>
-            </div>
+  const supabase = useSupabaseClient<Database>();
+  const user = useUser();
+  const [loading, setLoading] = useState(true);
+  const [fullName, setFullName] = useState<Profiles["full_name"]>(null);
+  const [phoneNumber, setPhoneNumber] = useState<Profiles["phone_number"]>(
+    null
+  );
+  const [editMode, setEditMode] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  useEffect(
+    () => {
+      getProfile();
+    },
+    [session]
+  );
+
+  async function getProfile() {
+    try {
+      setLoading(true);
+      if (!user) {
+        router.push("/");
+        return;
+      }
+
+      let { data, error, status } = await supabase
+        .from("profiles")
+        .select(`full_name, phone_number, avatar_url`)
+        .eq("id", user.id)
+        .single();
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setPhoneNumber(data.phone_number);
+        setFullName(data.full_name);
+      }
+    } catch (error) {
+      alert("Error loading user data!");
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function updateProfile({
+    phoneNumber
+  }: {
+    phoneNumber: Profiles["phone_number"];
+  }) {
+    try {
+      setLoading(true);
+      if (!user) {
+        router.push("/");
+        return;
+      }
+
+      const updates = {
+        id: user.id,
+        phone_number: phoneNumber,
+        updated_at: new Date().toISOString()
+      };
+
+      let { error } = await supabase.from("profiles").upsert(updates);
+      if (error) throw error;
+      alert("Profile updated!");
+    } catch (error) {
+      alert("Error updating the data!");
+      console.log(error);
+    } finally {
+      setLoading(false);
+      setEditMode(false);
+    }
+  }
+
+  return (
+    <div className="max-w-xl px-5 xl:px-0">
+      <h1
+        className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold  text-transparent drop-shadow-sm md:text-5xl md:leading-[5rem]"
+        style={{ opacity: 1, transform: "none" }}
+      >
+        <span
+          style={{
+            display: "inline-block",
+            verticalAlign: "top",
+            textDecoration: "inherit",
+            maxWidth: "541px"
+          }}
+        >
+          Hi {fullName || "There"},
+        </span>
+      </h1>
+      <p
+        className="mt-6 text-center text-gray-500 md:text-xl"
+        style={{ opacity: 1 }}
+      >
+        <span
+          style={{
+            display: "inline-block",
+            verticalAlign: "top",
+            textDecoration: "inherit",
+            maxWidth: "541px"
+          }}
+        >
+          Manage your schedule from WhatsApp by chatting with Arjun.
+        </span>
+      </p>
+      <div className="m-4">
+        <label className="block text-gray-700 font-bold mb-2">
+          Your phone number
+        </label>
+        <div className="flex items-center gap-5">
+          <PhoneInput
+            country={"us"}
+            value={phoneNumber}
+            onChange={phone => {
+              setPhoneNumber(phone);
+            }}
+            disabled={!editMode}
+            placeholder="Your WhatsApp number to chat with Arjun."
+          />
+          <button
+            onClick={() =>
+              editMode ? updateProfile({ phoneNumber }) : setEditMode(true)}
+            className="rounded-full border border-black bg-white p-1.5 px-4 text-sm text-black transition-all hover:bg-black hover:text-white self-center"
+          >
+            {editMode ? "Save" : "Edit"}
+          </button>
         </div>
-    );
+      </div>
+
+      <div
+        className="mx-auto mt-6 flex items-center justify-center space-x-5"
+        style={{ opacity: 1, transform: "none" }}
+      >
+        <a
+          href={`https://wa.me/+91${arjunWhatsAppNumber}?text=Hi+Arjun`}
+          className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black flex"
+        >
+          <FaWhatsapp className="mr-2" size={20} /> Chat with Arjun
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Account;
---
+++ src/types/database.types.ts
@@ -0,0 +1,83 @@
+export type Json=
+  |string
+  |number
+  |boolean
+  |null
+  |{ [key: string]: Json }
+  |Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      api_keys: {
+        Row: {
+          id: number
+          key: string
+          owner: string|null
+        }
+        Insert: {
+          id?: number
+          key: string
+          owner?: string|null
+        }
+        Update: {
+          id?: number
+          key?: string
+          owner?: string|null
+        }
+      }
+      profiles: {
+        Row: {
+          avatar_url: string|null
+          full_name: string|null
+          id: string
+          phone_number: string|null
+          updated_at: string|null
+        }
+        Insert: {
+          avatar_url?: string|null
+          full_name?: string|null
+          id: string
+          phone_number?: string|null
+          updated_at?: string|null
+        }
+        Update: {
+          avatar_url?: string|null
+          full_name?: string|null
+          id?: string
+          phone_number?: string|null
+          updated_at?: string|null
+        }
+      }
+      user_sessions: {
+        Row: {
+          expire: string|null
+          sess: Json|null
+          sid: string
+        }
+        Insert: {
+          expire?: string|null
+          sess?: Json|null
+          sid: string
+        }
+        Update: {
+          expire?: string|null
+          sess?: Json|null
+          sid?: string
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
