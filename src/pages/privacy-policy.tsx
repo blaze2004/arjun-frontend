bf12f5a6e97ea09fabaa1c0046207ba4df4af1af
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2023-03-19 23:53:23 +0530

    privacy policy imrpved spacings

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -12,11 +12,11 @@ const PrivacyPolicy=() => {
                         <span style={{ display: 'inline-block', verticalAlign: 'top', textDecoration: 'inherit', maxWidth: '541px' }}>
                             Arjun is a bot designed to help you manage your schedule and tasks. 
                             In order to perform these functions, the bot requires access to your Google Calendar and Tasks. 
-                            Please note that we don&apos;t do anything with your calendar and only perform operations which you ask Arjun to perform, such as creating or modifying events and tasks. 
+                            Please note that we don&apos;t do anything with your calendar and only perform operations which you ask Arjun to perform, such as creating or modifying events and tasks. <br /><br />
                             Arjun is designed to keep all of your conversations private. Your latest(very recent) chat with the bot will be stored on the server so that Arjun can remember the conversation history.
                             Please note that no data from your google account is stored on our servers other than tasks or events details included in conversation history, which stores only latest chat.
                             However, please note that this data is completely private and confidential.
-                            You can request to delete this data at any time. We don&apos;t do anything with your data other than to store it for conversation history purposes.
+                            You can request to delete this data at any time. We don&apos;t do anything with your data other than to store it for conversation history purposes. <br /><br />
                             We take the security of your data very seriously and have implemented appropriate technical and organizational measures to protect your personal information.
                             However, we cannot guarantee the security of your data when transmitted over the internet or when stored on servers not controlled by us.
                             By using Arjun, you consent to the terms and conditions of this Privacy Policy.
