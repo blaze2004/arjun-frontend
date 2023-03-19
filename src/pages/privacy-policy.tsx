7b9d86cc0939a016337130d42fcaa44bf275bd1c
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2023-03-20 00:32:46 +0530

    minor fix

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -21,7 +21,7 @@ const PrivacyPolicy=() => {
                             However, we cannot guarantee the security of your data when transmitted over the internet or when stored on servers not controlled by us. <br /><br />
                             Arjun complies with the Google API Services User Data Policy, including the Limited Use requirements. 
                             \By using Arjun, you consent to the terms and conditions of this policy.
-                            Arjun's use and transfer of information received from Google APIs to any other app will adhere to the Google API Services User Data Policy, including the Limited Use requirements.
+                            Arjun&apos;s use and transfer of information received from Google APIs to any other app will adhere to the Google API Services User Data Policy, including the Limited Use requirements.
                             You can access the Google API Services User Data Policy at any time from <a className="text-blue-800 hover:text-blue-500" href="https://developers.google.com/terms/api-services-user-data-policy">here</a>.
                             We reserve the right to modify this Privacy Policy at any time, so please review it frequently.
                             If you have any questions or concerns about this Privacy Policy, please contact us at <a className="text-blue-800 hover:text-blue-500" href="mailto:shubham@visualbrahma.tech">shubham@visualbrahma.tech</a>.
