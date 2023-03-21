e1b8b444d487db4138a69b0e15cf71ac5e6a75d3
Piyush Duggal <duggalpiyush0@gmail.com>
2023-03-21 18:02:03 +0530

    fixed login and dashboard UI

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,63 +5,85 @@ import { useRouter } from 'next/router';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 
 interface ComponentProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-const Layout: React.FC<ComponentProps>=({ children }) => {
-    const session=useSession();
-    const supabase=useSupabaseClient();
-    const router=useRouter();
-    return (
-        <div className="bg-gradient-to-br from-indigo-50 to-cyan-100 relative">
-            <Head>
-                <title>Arjun | WhatsApp Assistant</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <div className='fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100'></div>
-
-            <div className='absolute w-full h-screen bg-transparent'>
-                <div className="fixed top-0 w-full bg-white/0 z-30 transition-all">
-                    <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between xl:mx-auto">
-                        <Link className="flex items-center font-display text-2xl" href="/">
-                            {/* <img alt="Arjun logo" src="/logo.svg" decoding="async" className="mr-2 rounded-sm" loading="lazy" style={{ color: 'transparent' }} /> */}
-                            <p>Arjun</p>
-                        </Link>
-                        <div>
-                            <button onClick={() => (!session? router.push("/dashboard"):supabase.auth.signOut())} className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black" style={{ opacity: 1 }}>{!session? "Sign In":"Logout"}</button>
-                        </div>
-                    </div>
-                </div>
-
-                {children}
-
-                <div className="w-full border-t border-gray-200 bg-white py-5 text-center self-end">
-                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row justify-between items-center">
-                        <div className="text-center sm:text-left">
-                            <p className="text-sm">
-                                &copy; 2023 Arjun. All rights reserved.
-                            </p>
-                        </div>
-                        <div className="mt-4 sm:mt-0">
-                            <Link
-                                href="/privacy-policy"
-                                className="text-gray-800 hover:text-gray-300 ml-4"
-                            >
-                                Privacy Policy
-                            </Link>
-                            <a
-                                href="mailto:shubhamtiwari06112004+arjun@gmail.com"
-                                className="text-gray-800 hover:text-gray-300 ml-4"
-                            >
-                                Contact Us
-                            </a>
-                        </div>
-                    </div>
-                </div>
+const Layout: React.FC<ComponentProps> = ({ children }) => {
+  const session = useSession();
+  const supabase = useSupabaseClient();
+  const router = useRouter();
+  const scopes = [
+    'https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/calendar.events',
+    'https://www.googleapis.com/auth/tasks',
+    'https://www.googleapis.com/auth/tasks.readonly',
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile',
+  ];
+  return (
+    <div className='bg-gradient-to-br from-indigo-50 to-cyan-100 relative'>
+      <Head>
+        <title>Arjun | WhatsApp Assistant</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className='fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100'></div>
+      <div className='absolute w-full h-screen bg-transparent'>
+        <div className='fixed top-0 w-full bg-white/0 z-30 transition-all'>
+          <div className='mx-5 flex h-16 max-w-screen-xl items-center justify-between xl:mx-auto'>
+            <Link className='flex items-center font-display text-2xl lg:text-3xl font-bold' href='/'>
+              {/* <img alt="Arjun logo" src="/logo.svg" decoding="async" className="mr-2 rounded-sm" loading="lazy" style={{ color: 'transparent' }} /> */}
+              Arjun
+            </Link>
+            <div>
+              <button
+                onClick={() =>
+                  !session
+                    ? router.push(
+                        `${
+                          process.env.NEXT_PUBLIC_SUPABASE_URL
+                        }/auth/v1/authorize?provider=google&scopes=${scopes.join(
+                          ' '
+                        )}&redirect_to=${location.protocol}//${
+                          location.host
+                        }/dashboard&prompt=consent&access_type=offline`
+                      )
+                    : supabase.auth.signOut()
+                }
+                className='button-1'
+                style={{ opacity: 1 }}
+              >
+                {!session ? 'Sign In' : 'Logout'}
+              </button>
             </div>
+          </div>
         </div>
-    )
-}
+
+        {children}
+
+        <div className='w-full border-t border-gray-200 bg-white py-5 text-center self-end'>
+          <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row justify-between items-center'>
+            <div className='text-center sm:text-left'>
+              <p className='text-sm'>&copy; 2023 Arjun. All rights reserved.</p>
+            </div>
+            <div className='mt-4 sm:mt-0'>
+              <Link
+                href='/privacy-policy'
+                className='text-gray-800 hover:text-gray-300 ml-4'
+              >
+                Privacy Policy
+              </Link>
+              <a
+                href='mailto:shubhamtiwari06112004+arjun@gmail.com'
+                className='text-gray-800 hover:text-gray-300 ml-4'
+              >
+                Contact Us
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,36 @@
 import Layout from '@/components/layout';
 import Link from 'next/link';
 
-const LandingPage=() => {
+const LandingPage = () => {
   return (
     <Layout>
-      <main className="flex w-full flex-col items-center justify-center py-32 h-screen">
-        <div className='max-w-xl px-5 xl:px-0'>
-          <h1 className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold  text-transparent drop-shadow-sm md:text-7xl md:leading-[5rem]" style={{ opacity: 1, transform: "none" }}>
-            <span style={{ display: 'inline-block', verticalAlign: 'top', textDecoration: 'inherit', maxWidth: '541px' }}>Your Personal Assistant On WhatsApp</span>
+      <main className='flex w-full flex-col items-center justify-center py-32 h-screen'>
+        <div className='mb-6'>
+          <h1
+            className='bg-gradient-to-br from-blue-400 mb-4 to-green-500 bg-clip-text text-center font-display text-4xl font-bold text-transparent drop-shadow-sm md:text-7xl'
+          >
+              Your Personal Assistant <br/> On WhatsApp
           </h1>
-          <p className="mt-6 text-center text-gray-500 md:text-xl" style={{ opacity: 1 }}>
-            <span style={{ display: 'inline-block', verticalAlign: 'top', textDecoration: 'inherit', maxWidth: '541px' }}>Manage your schedule from WhatsApp by chatting with Arjun.<br />Arjun is currently in Alpha phase and might give wrong responses.</span>
+          <p
+            className='text-center text-gray-500 mx-2 md:text-xl'
+          >
+              Manage your schedule from WhatsApp while chatting with Arjun.
           </p>
-          <div className="mx-auto mt-6 flex items-center justify-center space-x-5" style={{ opacity: 1, transform: "none" }}>
-            <Link
-              href="/dashboard"
-              className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
-            >
+        </div>
+          <div className='rounded-full text-center py-0.5 px-5 mt-5 mx-2 text-[12px] lg:text-sm text-red-800 ring-1 ring-red-500'>
+            Arjun is currently in Alpha phase and might give wrong responses.
+          </div>
+          <div
+            className='mx-auto mt-6 flex items-center justify-center space-x-5'
+            style={{ opacity: 1, transform: 'none' }}
+          >
+            <Link href='/dashboard' className='button-2'>
               Get started
             </Link>
           </div>
-        </div>
       </main>
     </Layout>
   );
-}
+};
 
 export default LandingPage;
